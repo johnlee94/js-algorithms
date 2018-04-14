b6f945d951f2a0d05696bbf8e0db75bbac2eaa15
john_lee <jjlee099@gmail.com>
2018-04-13 23:49:10 -0700

    adds end note about binary search

--- js-data-structures/binary-search.js
+++ js-data-structures/binary-search.js
@@ -21,3 +21,10 @@ function binarySearch(sortedArray, key) {
 var myArray = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 30, 34, 36, 40, 51]
 
 binarySearch(myArray, 17); // => 1 (Since 17 is the second element in the sorted Array)
+
+// some notes about binary search...
+// Logarithmic Time Complexity O(log n), well technically O(n log n) since you have to
+// sort the Array first in order to use binary search
+// note that once you sort the data once, you can continue to use binary search on it
+// -- meaning that you should insert data into the sorted array, keeping it sorted if
+// you want to continue utilizing binary search on it!
