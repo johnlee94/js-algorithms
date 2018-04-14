6bddeb57c712e4d8a0437f2e9d40d552d69e2a3e
john_lee <jjlee099@gmail.com>
2018-04-13 17:37:07 -0700

    harmless-ransom-note

---
+++ harmless-ransom-note.js
@@ -0,0 +1,16 @@
+// function that takes 2 parameters: 1, the note (a String) and 2, the
+// 'magazine' text (also a String) that may have words needed to compose the note
+// --if the magazine has enough of the proper words to compose the note it should
+// return true, and if not, should return false (note that each word from the
+// magazine text can only be used once!)
+
+// i.e. NOTE: 'this is a secret note for you from a secret admirer'
+// MAGAZINE TEXT: 'puerto rico IS A great place YOU must hike FAR FROM town to find
+// A SECRET waterfall that i am an ADMIRER of but note that it is not as hard as it
+// seems THIS is my advice for you'
+// => returns false because 'SECRET' is found only one time inside the magazine text
+
+
+function harmlessRansomNote(noteText, magazineText) {
+
+}
