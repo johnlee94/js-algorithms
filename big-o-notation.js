3ee7ce0660a65e4bc5fa836a460f6d84a2528c62
john_lee <jjlee099@gmail.com>
2018-04-13 19:05:48 -0700

    Big O Notation

--- big-o-notation.js
+++ big-o-notation.js
@@ -44,3 +44,38 @@ function addAndLog(array) {
 addAndLog(['A', 'B', 'C']); // 9 pairs logged out (input of 3)
 addAndLog(['A', 'B', 'C', 'D']); // 16 pairs logged out (input of 4)
 addAndLog(['A', 'B', 'C', 'D', 'E']); // 25 pairs logged out (input of 5)
+
+
+// ***Logarithmic runtime: Big O Notation: "O (log n)"
+// as our input size grows, our number of operations will grow logarthmically
+// meaning less than proportionally-- even if input grows a lot, number of operations
+// required to complete the function won't grow that much!
+
+// aside: binary search is like finding a word in the dictionary: you cut your sorted
+// list in half and throw away the half that does not have the key you are looking for
+// and keep cutting in half until you have the key
+
+//i.e. 4000 elements -> only 12 operations!
+function binarySearch(sortedArray, key) {
+  var low = 0;
+  var high = sortedArray.length - 1;
+  var mid;
+  var element;
+
+  while (low <= high) {
+    mid = Math.floor((low + high) / 2, 10);
+    element = sortedArray[mid];
+    if (element < key) {
+      low = mid + 1;
+    } else if (element > key) {
+      high = mid -1;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+}
+
+var myArray = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 30, 34, 36, 40, 51]
+
+binarySearch(myArray, 17); // => 1 (Since 17 is the second element in the sorted Array)
