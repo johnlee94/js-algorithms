d6633b84aaae0302acb02e8292709c3ab30264df
john_lee <jjlee099@gmail.com>
2018-04-15 12:27:07 -0700

    booyea course is same as mine

--- reverse-words.js
+++ reverse-words.js
@@ -67,3 +67,21 @@ function reverseWords(string) {
 
 reverseWords('test is true') // => returns 'tset si ture'
 // I CAN't FIGURE OUT WHY THE LAST WORD ISN'T BEING PROPERLY REVERSED?!
+
+// Course's
+function reverseWords(string) {
+  let wordsArr = string.split(' ');
+  let reversedWordsArr = [];
+
+  wordsArr.forEach(word => {
+    let reversedWord = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+      reversedWord += word[i];
+    }
+    reversedWordsArr.push(reversedWord);
+  });
+
+  return reversedWordsArr.join(' ');
+}
+
+reverseWords('this is a string of words'); // => 'siht si a gnirts fo sdrow'
