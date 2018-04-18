799ec8996b9b818b3cd1c516d6dfb96c689cb9c7
john_lee <jjlee099@gmail.com>
2018-04-17 19:03:19 -0700

    binary search with recursion

--- binary-search.js
+++ binary-search.js
@@ -1,3 +1,15 @@
 function binarySearch(numArray, key) {
-  
+  let middleIndex = Math.floor(numArray.length / 2);
+  let middleElem = numArray[middleIndex];
+
+  if (middleElem === key) return true;
+  else if (middleElem < key && numArray.length > 1) {
+    return binarySearch(numArray.splice(middleIndex, numArray.length), key);
+  }
+  else if (middleElem > key && numArray.length > 1) {
+    return binarySearch(numArray.splice(0, middleIndex),key);
+  }
+  else return false;
 }
+
+binarySearch([5, 7, 12, 16, 36, 39, 42, 56, 71], 56);
