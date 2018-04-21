8c64a05e28d8fff182f6fc97be61453b2716d7df
john_lee <jjlee099@gmail.com>
2018-04-20 23:44:24 -0700

    IFFE

--- mock-interview-questions/iife.js
+++ mock-interview-questions/iife.js
@@ -1,7 +1,8 @@
 // what is an IIFE and why are they used?
 // code out an example of an IIFE
 
-IIFE is an Immediately Invoked Function Expression
+IIFE is an Immediately Invoked Function Expression,
+or a function that is executed right after it is created
 
 (function()) => They are enclosed in Parantheses.
  "Grouping Operator ()" or Lexical Scope
@@ -12,4 +13,26 @@ Notes: Variables stored within IIFE's cannot be accessed from outside
 // i.e.
 (function Alert() {
   alert("Hi There!");
-})
+})();
+
+
+// course's
+
+(function doubleNumber(num) {
+  return num * 2;
+})(10)
+
+// the main purpose of using IIFE's is to preserve variables (not ovewrite global variables)
+// i.e. if using jQuery, var $ = '...' can never be used or it will overwrite
+
+// a neat trick is to wrap an entire javascript file into an iffe to give it
+// its own private scope!
+(function() {
+
+ // CODE //
+ // FUNCTIONS //
+ // VARIABLES //
+ // OPERATIONS //
+ var $ = 'currency';
+
+})();
