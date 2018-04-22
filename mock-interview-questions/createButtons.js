b8081762fb9e4fbd5eb7b7e5e10519f5d1118944
john_lee <jjlee099@gmail.com>
2018-04-22 13:36:52 -0700

    create buttons

--- mock-interview-questions/createButtons.js
+++ mock-interview-questions/createButtons.js
@@ -14,3 +14,27 @@ createButtons();
 
 // => This creates 5 buttons, but on click, it returns 'This is button 6'
 // => Fix it, and why is it doing it?
+
+// course
+
+
+// the button prints out 'This is button 6' because the buttons are created at
+// each iterator but the onclick function is changed after the loop is finished
+// Also, it prints out 6 because the loop breaks after i iterates to 6
+
+// if you use an IFFE, you can store each i as it iterates which fixes the problem
+function createButtons() {
+   for (var i = 1; i <= 5; i++) {
+     var body = document.getElementsByTagName("BODY")[0];
+   var button = document.createElement("BUTTON");
+     button.innerHTML = 'Button ' + i;
+     (function(num) {
+       button.onclick = function() {
+         alert('This is button ' + i);
+       };
+     })(i);
+     body.appendChild(button);
+   }
+}
+
+createButtons();
