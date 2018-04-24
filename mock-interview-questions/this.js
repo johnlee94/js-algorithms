5b50f45cbe2784aaee84e52a7faa46db2c6ff7c0
john_lee <jjlee099@gmail.com>
2018-04-23 17:20:17 -0700

    this

--- mock-interview-questions/this.js
+++ mock-interview-questions/this.js
@@ -17,3 +17,20 @@ var John = new Person('John', 23);
 
 
 // course's
+// the 'this' keyword is used to refer to the object that it is inside of
+
+// if you console.log(this) in the console, it returns the global Window object
+// because it isn't located in another object
+
+var house = {
+  price: 100000,
+  squareFeet: 2000,
+  owner: 'Taylor',
+  getPricePerSquareFoot: function() {
+    // same as return house.price / house.squareFeet;
+    return this.price / this.squareFeet;
+  }
+};
+
+console.log(house.price);
+console.log(house.getPricePerSquareFoot);
